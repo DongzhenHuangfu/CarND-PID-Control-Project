1f09fdf6fd0a5f4ea1ad66cb9591090832c3a2f6
Dongzhen <wyatt_h@outlook.com>
2018-08-23 16:37:31 +0200

    feat: finish PID controller.

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,31 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double _Kp, double _Ki, double _Kd) {
+
+	Kp = _Kp;
+	Ki = _Ki;
+	Kd = _Kd;
+	i_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+
+	d_error = cte - p_error;
+	p_error = cte;
+	i_error += cte;
 }
 
 double PID::TotalError() {
+
+	double steer_value = - Kp * p_error - Ki * i_error - Kd * d_error;
+	if(steer_value < -1){
+		steer_value = -1;
+	}
+    
+    else if(steer_value > 1){
+      steer_value = 1;
+    }
+	return steer_value; 
 }
 
